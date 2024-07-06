7aa0c849a1142b3a6a52c59431667efe1cb731d5
Otto Fernandes <ottofernandes2012@gmail.com>
2024-07-06 19:35:47 -0300

    Add mydos with new name, LDOS

---
+++ exercises/mydos/bios2.h
@@ -0,0 +1,19 @@
+/*
+ *    SPDX-FileCopyrightText: 2024 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2024 Otto Fernandes <ottofernandes2012@gmail.com>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative work from SYSeg (https://gitlab.com/monaco/syseg)
+ *  and contains modifications carried out by the following author(s):
+ *  Otto Fernandes <ottofernandes2012@gmail.com>
+ */
+
+#ifndef BIOS2_H
+#define BIOS2_H
+
+int __attribute__((fastcall)) kread(char *);
+
+void __attribute__((fastcall)) udelay(unsigned short);
+
+#endif  /* BIOS2_H  */
