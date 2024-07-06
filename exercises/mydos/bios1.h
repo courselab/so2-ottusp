d163492ba4982d58148ab67d2fc53e6b3c8be8dc
Otto Fernandes <ottofernandes2012@gmail.com>
2024-07-06 19:35:47 -0300

    Add mydos with new name, LDOS

---
+++ exercises/mydos/bios1.h
@@ -0,0 +1,23 @@
+/*
+ *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2024 Otto Fernandes <ottofernandes2012@gmail.com>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative work from SYSeg (https://gitlab.com/monaco/syseg)
+ *  and contains modifications carried out by the following author(s):
+ *  Otto Fernandes <ottofernandes2012@gmail.com>
+ */
+
+#ifndef BIOS1_H
+#define BIOS1_H
+
+void __attribute__((fastcall)) clear (void);
+void __attribute__((fastcall)) kwrite(const char*);
+void __attribute__((fastcall)) kwriteln(const char*);
+/* void __attribute__((fastcall)) kread(char *); */
+void __attribute__((fastcall)) fatal(const char*);
+void __attribute__((fastcall)) load_kernel(void);
+void __attribute__((fastcall)) set_cursor(char, char);
+
+#endif
---
+++ exercises/mydos/prog.c
@@ -0,0 +1,17 @@
+/*
+ *    SPDX-FileCopyrightText: 2024 Otto Fernandes <ottofernandes2012@gmail.com>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative work from SYSeg (https://gitlab.com/monaco/syseg)
+ *  and contains modifications carried out by the following author(s):
+ *  Otto Fernandes <ottofernandes2012@gmail.com>
+ */
+
+#include "tydos.h"
+
+int main()
+{
+  puts ("Hello World\n");
+  return 0;
+}
